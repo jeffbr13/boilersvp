15f3a0309ef5ccfe35854aab7f1cac59a945c5f1
Ben Jeffrey <mail@benjeffrey.net>
2017-08-30 08:54:52 +0100

    Define model string representations.

--- boilersvp/subscriptions/models.py
+++ boilersvp/subscriptions/models.py
@@ -4,12 +4,21 @@ from django.db import models
 class City(models.Model):
     name = models.CharField(max_length=200)
 
+    def __str__(self):
+        return self.name
+
 
 class Event(models.Model):
     name = models.CharField(max_length=300)
     start = models.DateTimeField()
 
+    def __str__(self):
+        return self.name
+
 
 class Subscriber(models.Model):
     email = models.EmailField()
     cities = models.ManyToManyField(City)
+
+    def __str__(self):
+        return self.email
