197cbaa48687d521bfc245d09b31b19acfbe8ce8
Ben Jeffrey <mail@benjeffrey.net>
2017-09-11 13:41:44 +0100

    Use generic email envvar names.

--- boilersvp/boilersvp/settings.py
+++ boilersvp/boilersvp/settings.py
@@ -141,10 +141,10 @@ if DEBUG:
     EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 else:
     EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-    EMAIL_HOST = env.str('MAILGUN_SMTP_SERVER')
-    EMAIL_PORT = env.str('MAILGUN_SMTP_PORT')
-    EMAIL_HOST_USER = env.str('MAILGUN_SMTP_LOGIN')
-    EMAIL_HOST_PASSWORD = env.str('MAILGUN_SMTP_PASSWORD')
+    EMAIL_HOST = env.str('EMAIL_HOST')
+    EMAIL_PORT = env.str('EMAIL_PORT')
+    EMAIL_HOST_USER = env.str('EMAIL_HOST_USER')
+    EMAIL_HOST_PASSWORD = env.str('EMAIL_HOST_PASSWORD')
     EMAIL_USE_TLS = True
 
 # Password validation
